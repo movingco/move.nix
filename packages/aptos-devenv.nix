50f1400ac3ef79e3ab13d803dac75f6d42a6d281
Ian Macalinao <ian@moving.wtf>
2022-07-01 13:21:07 -0500

    make aptos devenv work

--- packages/aptos-devenv.nix
+++ packages/aptos-devenv.nix
@@ -3,13 +3,23 @@ with pkgs;
 mkShell {
   name = "aptos-devenv";
 
+  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+  # see https://github.com/NixOS/nixpkgs/issues/52447#issuecomment-852079285
+  LIBCLANG_PATH = "${libclang.lib}/lib";
+  BINDGEN_EXTRA_CLANG_ARGS =
+    "-isystem ${libclang.lib}/lib/clang/${lib.getVersion clang}/include";
+
+  nativeBuildInputs = with pkgs; [ rustc cargo clang ];
+
   # see: https://github.com/aptos-labs/aptos-core/blob/36dfc6499dd576d7d2ba883b66161510ff5cbe6b/.circleci/config.yml#L241
   buildInputs = with pkgs; [
     pkg-config
+    libclang
+    rocksdb
     postgresql # libpq
     openssl # libssl
-
-    rustup
 
     cargo-hakari # for workspace hack
   ] ++ (
