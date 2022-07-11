2ea0a1c012ba0b07d11bd0c714de81ac3bb008b3
Ian Macalinao <ian@moving.wtf>
2022-07-11 18:38:20 -0500

    Upgrade Aptos to latest devnet

--- packages/aptos/cli.nix
+++ packages/aptos/cli.nix
@@ -8,16 +8,16 @@ in
 buildAptos rec {
   pname = "aptos-cli";
   # https://github.com/movingco/aptos-core/tree/devnet-for-nix
-  version = "a669724b5b3df1c2d2b1a6b25c31f4cad5177229";
+  version = "a0ba71f4f02d403ce3222bc9a87aa3f2df95f49b";
 
   src = fetchFromGitHub {
     owner = "movingco";
     repo = "aptos-core";
     rev = version;
-    sha256 = "sha256-i2czGZSZHMi0Ejqeh9X6n8ydpkikgRyOOGNHMEKbtmE=";
+    sha256 = "sha256-1gj5jZ59cjeJocUaloJ1aH2c6iQAKPi3hzRaLruakJk=";
   };
 
-  cargoSha256 = "sha256-paR5KXvUVbQQeI0l92Qiav8RX6ok1IzWvcm/qWEkLVI=";
+  cargoSha256 = "sha256-paR5KXvUVbQQeI0l92Qiav8RX6ok2IzWvcm/qWEkLVI=";
 
   buildAndTestSubdir = "crates/aptos";
 }
--- packages/aptos/full.nix
+++ packages/aptos/full.nix
@@ -7,14 +7,14 @@ let
 in
 buildAptos rec {
   # https://github.com/movingco/aptos-core/tree/devnet-for-nix
-  version = "a669724b5b3df1c2d2b1a6b25c31f4cad5177229";
+  version = "a0ba71f4f02d403ce3222bc9a87aa3f2df95f49b";
 
   src = fetchFromGitHub {
     owner = "movingco";
     repo = "aptos-core";
     rev = version;
-    sha256 = "sha256-i2czGZSZHMi0Ejqeh9X6n8ydpkikgRyOOGNHMEKbtmE=";
+    sha256 = "sha256-1gj5jZ59cjeJocUaloJ1aH2c6iQAKPi3hzRaLruakJk=";
   };
 
-  cargoSha256 = "sha256-bvUEaUsw+lO0leaCAztWknsJneJIHikeT7x2r78olOo=";
+  cargoSha256 = "sha256-awAZECBHFfKNVwvPON0moglSy6MGT/tSH1cF8K4vwgI=";
 }
