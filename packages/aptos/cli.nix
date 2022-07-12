b980d3909c23c31badc78dba7b736db56c84ff76
Ian Macalinao <ian@moving.wtf>
2022-07-11 23:35:57 -0500

    more cleanup

--- packages/aptos/cli.nix
+++ packages/aptos/cli.nix
@@ -1,23 +1,5 @@
-{ callPackage, fetchFromGitHub, cargo-hakari }:
-
-let
-  buildAptos = callPackage ./aptos.nix {
-    inherit cargo-hakari;
-  };
-in
-buildAptos rec {
+import ./aptos-devnet.nix {
   pname = "aptos-cli";
-  # https://github.com/movingco/aptos-core/tree/devnet-for-nix
-  version = "a0ba71f4f02d403ce3222bc9a87aa3f2df95f49b";
-
-  src = fetchFromGitHub {
-    owner = "movingco";
-    repo = "aptos-core";
-    rev = version;
-    sha256 = "sha256-1gj5jZ59cjeJocUaloJ1aH2c6iQAKPi3hzRaLruakJk=";
-  };
-
   cargoSha256 = "sha256-lzu9MtgPm0+jd9Q60MwBPOcYvo2jAPTPLz6pAglK0KI=";
-
   buildAndTestSubdir = "crates/aptos";
 }
--- packages/aptos/full.nix
+++ packages/aptos/full.nix
@@ -1,20 +1,4 @@
-{ callPackage, fetchFromGitHub, cargo-hakari }:
-
-let
-  buildAptos = callPackage ./aptos.nix {
-    inherit cargo-hakari;
-  };
-in
-buildAptos rec {
-  # https://github.com/movingco/aptos-core/tree/devnet-for-nix
-  version = "a0ba71f4f02d403ce3222bc9a87aa3f2df95f49b";
-
-  src = fetchFromGitHub {
-    owner = "movingco";
-    repo = "aptos-core";
-    rev = version;
-    sha256 = "sha256-1gj5jZ59cjeJocUaloJ1aH2c6iQAKPi3hzRaLruakJk=";
-  };
-
+import ./aptos-devnet.nix {
+  pname = "aptos";
   cargoSha256 = "sha256-awAZECBHFfKNVwvPON0moglSy6MGT/tSH1cF8K4vwgI=";
 }
