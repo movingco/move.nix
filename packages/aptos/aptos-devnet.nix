dad8c098b76c153a5186bbd387c5f9a3456b3aa3
Ian Macalinao <ian@moving.wtf>
2022-08-03 14:18:00 +0900

    Update to latest Aptos devnet

--- packages/aptos/aptos-devnet.nix
+++ packages/aptos/aptos-devnet.nix
@@ -8,14 +8,14 @@ let
   buildAptosDevnet = { pname, cargoSha256, buildAndTestSubdir ? null }:
     buildAptos rec {
       inherit pname;
-      # https://github.com/movingco/aptos-core/tree/devnet-for-nix-2
-      version = "58d09aa9434936b108e9ba562fe52d34f58e2023";
+      # https://github.com/movingco/aptos-core/tree/devnet-nix-2022-08-03
+      version = "7296f138e11113fab6a1e193fc3f18fa7d08bd3e";
 
       src = fetchFromGitHub {
         owner = "movingco";
         repo = "aptos-core";
         rev = version;
-        sha256 = "sha256-9Atz9deDeD9D5V6zotpR/r5Gf298Ukc7HgEwESuIreE=";
+        sha256 = "sha256-9Atz9eeDeD9D5V6zotpR/r5Gf298Ukc7HgEwESuIreE=";
       };
 
       inherit cargoSha256 buildAndTestSubdir;
@@ -24,18 +24,12 @@ in
 {
   cli = buildAptosDevnet {
     pname = "aptos-cli";
-    cargoSha256 = "sha256-xgk41K61veaRJMAAigChrT6CytbAlVwk2nJXgTK5pBM=";
+    cargoSha256 = "sha256-xgk41L61veaRJMAAigChrT6CytbAlVwk2nJXgTK5pBM=";
     buildAndTestSubdir = "crates/aptos";
   };
 
   full = buildAptosDevnet {
     pname = "aptos";
-    cargoSha256 = "sha256-eaVBwhD3aWw3bak/3mMLfJ1GbPR5J5fdwA6PyBfDmmw=";
-  };
-
-  af-cli = buildAptosDevnet {
-    pname = "af-cli";
-    cargoSha256 = "sha256-/TYeR0N4tcd16Yy1ulgx7rT03QjrKXUWwwPCrrxeUlk=";
-    buildAndTestSubdir = "aptos-move/af-cli";
+    cargoSha256 = "sha256-eaVBwhD3bWw3bak/3mMLfJ1GbPR5J5fdwA6PyBfDmmw=";
   };
 }
