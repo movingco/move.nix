25a6121019cab7885d091493082b52d885c9c23b
Ian Macalinao <ian@moving.wtf>
2022-08-16 14:52:30 +0900

    update aptos shell

--- shells/aptos.nix
+++ shells/aptos.nix
@@ -4,36 +4,17 @@ with pkgs;
 let
   installProver = !stdenv.isi686;
 in
-stdenv.mkDerivation
-  ({
-    name = "devshell-aptos";
+stdenv.mkDerivation {
+  name = "devshell-aptos";
 
-    # See: <https://github.com/move-language/move/blob/2dabc08cb115a2385d3844b681917dd3129a30fe/scripts/dev_setup.sh>
-    buildInputs = [
-      nixpkgs-fmt
-      move-cli-address32
-      move-ts-aptos
-      aptos
+  # See: <https://github.com/move-language/move/blob/2dabc08cb115a2385d3844b681917dd3129a30fe/scripts/dev_setup.sh>
+  buildInputs = [
+    nixpkgs-fmt
+    move-cli-address32
+    move-ts-aptos
+    aptos
 
-      # Git is needed to be able to clone packages
-      git
-    ] ++ (lib.optionals installProver [
-      # Move prover
-      z3
-      # TODO(igm): boogie must be installed via `dotnet tool install --global boogie`.
-      # boogie
-      icu
-      cvc5
-      dotnet-sdk
-    ]);
-  } // (if installProver then {
-    LD_LIBRARY_PATH = "${icu}/lib";
-    Z3_EXE = "${z3}/bin/z3";
-    DOTNET_ROOT = "${dotnet-sdk}";
-    shellHook = ''
-      export BOOGIE_EXE=$HOME/.dotnet/tools/boogie
-      if ! [ -f "$BOOGIE_EXE" ]; then
-        dotnet tool install --global boogie
-      fi
-    '';
-  } else { }))
+    # Git is needed to be able to clone packages
+    git
+  ];
+}
