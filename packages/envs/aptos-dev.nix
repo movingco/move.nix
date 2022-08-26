581734e16158c6d94ead84f268e146852d5e4191
Ian Macalinao <ian@moving.wtf>
2022-08-26 10:01:12 -0500

    use rustup in aptos devenv

--- packages/envs/aptos-dev.nix
+++ packages/envs/aptos-dev.nix
@@ -1,12 +1,10 @@
 # Useful for working in the Aptos repo.
 { lib
+, rustup
 , cargo-hakari
 , openssl
 , rust
-, libclang
-, clang
-, rustc
-, cargo
+, rust-analyzer
 , pkg-config
 , rocksdb
 , postgresql
@@ -19,23 +17,17 @@
 mkShell {
   name = "env-aptos-dev";
 
-  PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
   RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
-
-  # see https://github.com/NixOS/nixpkgs/issues/52447#issuecomment-852079285
-  LIBCLANG_PATH = "${libclang.lib}/lib";
-  BINDGEN_EXTRA_CLANG_ARGS =
-    "-isystem ${libclang.lib}/lib/clang/${lib.getVersion clang}/include";
-
-  nativeBuildInputs = [ rustc cargo clang ];
 
   # see: https://github.com/aptos-labs/aptos-core/blob/36dfc6499dd576d7d2ba883b66161510ff5cbe6b/.circleci/config.yml#L241
   buildInputs = [
     pkg-config
-    libclang
     rocksdb
     postgresql # libpq
     openssl # libssl
+
+    rustup
+    rust-analyzer
 
     cargo-hakari # for workspace hack
     move-cli-address32 # move-analyzer
