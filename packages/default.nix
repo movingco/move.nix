e185459051d45e65ec7eda0e94cd40f5aac00a79
Ian Macalinao <ian@moving.wtf>
2022-09-02 15:21:27 -0500

    Update Aptos (#12)

--- packages/default.nix
+++ packages/default.nix
@@ -1,31 +1,31 @@
 { pkgs }:
 with pkgs;
 rec {
-  cargo-hakari = pkgs.callPackage ./cargo-hakari.nix { };
-  sui-devnet = pkgs.callPackage ./sui/sui-devnet.nix { };
-  aptos-devnet = pkgs.callPackage ./aptos/aptos-devnet.nix { };
+  cargo-hakari = callPackage ./cargo-hakari.nix { };
+  sui-devnet = callPackage ./sui/sui-devnet.nix { };
 
   sui = sui-devnet.full;
   sui-cli = sui-devnet.cli;
   sui-gateway = sui-devnet.gateway;
 
-  move-ts = pkgs.callPackage ./move-ts.nix { };
+  move-ts = callPackage ./move-ts.nix { };
   move-ts-sui = move-ts.override { buildFeatures = [ "address20" ]; };
   move-ts-aptos = move-ts.override { buildFeatures = [ "address32" ]; };
-  z3 = pkgs.callPackage ./z3.nix { };
-  wrapWithProver = pkgs.callPackage ./wrapWithProver.nix { };
-  move-to-ts = pkgs.callPackage ./move-to-ts.nix { };
+  z3 = callPackage ./z3.nix { };
+  wrapWithProver = callPackage ./wrapWithProver.nix { };
+  move-to-ts = callPackage ./move-to-ts.nix { };
+
+  inherit (callPackages ./aptos {
+    inherit (darwin.apple_sdk.frameworks) DiskArbitration Foundation;
+  }) aptos aptos-node aptos-tools aptos-full;
 
   inherit (callPackages ./move-cli.nix {
     inherit (darwin.apple_sdk.frameworks) IOKit Security CoreFoundation AppKit System;
   }) move-cli move-cli-address20 move-cli-address32;
 
-  aptos-cli = aptos-devnet.cli;
-  aptos = aptos-devnet.full;
+  cargo-workspaces = callPackage ./cargo-workspaces.nix { };
 
-  cargo-workspaces = pkgs.callPackage ./cargo-workspaces.nix { };
-
-  env-rust = pkgs.callPackage ./envs/rust.nix { };
-  env-aptos-dev = pkgs.callPackage ./envs/aptos-dev.nix { };
-  env-move = pkgs.callPackage ./envs/move.nix { };
+  env-rust = callPackage ./envs/rust.nix { };
+  env-aptos-dev = callPackage ./envs/aptos-dev.nix { };
+  env-move = callPackage ./envs/move.nix { };
 }
